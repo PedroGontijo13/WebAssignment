0d25a8d5cabd87484ee1e01f304f3afd153050d1
Pedro Gontijo <pedrotolentino2002@gmail.com>
2022-11-06 17:13:01 -0800

    AssignmentsJs ✅

---
+++ JavaScriptAssignment/assignment3/script.js
@@ -0,0 +1,52 @@
+const table = `
+<div class="center margin-top">
+  <table>
+    <tr>
+      <th>Review scores</th>
+      <th>Score</th>
+    </tr>
+    <tr>
+      <td>EGM</td>
+      <td>4/5</td>
+    </tr>
+    <tr>
+      <td>Game Informer</td>
+      <td>8/10</td>
+    </tr>
+    <tr>
+      <td>GameSpot</td>
+      <td>9/10</td>
+    </tr>
+    <tr>
+      <td>GamesRadar+</td>
+      <td>4.5/5</td>
+    </tr>
+    <tr>
+      <td>Ign</td>
+      <td>8/10</td>
+    </tr>
+    <tr>
+      <td>Jeuxvideo.com</td>
+      <td>15/20</td>
+    </tr>
+    <tr>
+      <td>PC Gamer (US)</td>
+      <td>82/100</td>
+    </tr>
+    <tr>
+      <td>PCGamesN</td>
+      <td>9/10</td>
+    </tr>
+    <tr>
+      <td>The Guardian</td>
+      <td>5/5</td>
+    </tr>
+    <tr>
+      <td>Video Games Chronicle</td>
+      <td>5/5</td>
+    </tr>
+  </table>
+</div>
+`;
+
+document.getElementById('table').innerHTML += table
